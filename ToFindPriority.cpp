bf3119f1a58d9116bf45addcde5f47f6f9f33cfb
Sushant Bansal <1star.sushant@gmail.com>
2014-11-27 22:21:17 +0530

    To Find Priority !!
    It's working now , but need a full stop at the end of every task.

--- ToFindPriority.cpp
+++ ToFindPriority.cpp
@@ -1,50 +1,129 @@
 #include <iostream>
 #include<fstream>
-#include<string>
+#include<string.h>
 #include<windows.h>
 #include<conio.h>
 #include<stdio.h>
-
+//#include<graphics.h>
 
 
 using namespace std;
-
-int CountWord()
+int storePriority[8];
+int x=0;
+void CountWord()
 {
-    //ofstream a("list.txt",ios::out | ios::app | ios::binary);
-    //a<<"awesome"<<endl;
-    //ofstream b("10.txt",ios::out | ios::app | ios::binary);
-    //b<<"awesome"<<endl;
+    //ofstream a("9.txt",ios::out | ios::app | ios::binary);
+    //ofstream b("8.txt",ios::out | ios::app | ios::binary);
+    //a<<"i"<<endl;
+    //b<<"am"<<endl;
     ifstream usernameObj("list.txt",ios::in|ios::binary);
     string strname;
     int chck=0;
+
+    int priorityCount=0;
     while(usernameObj >> strname)
     {
-        //cout<<strname;
-        ifstream dataFile("10.txt",ios::in | ios::binary);
         string wordchk;
+        char check;
 
+        ifstream dataFile10("10.txt",ios::in | ios::binary);
 
-        while(dataFile >> wordchk)
+        while(dataFile10 >> wordchk)
+        {
+            if(wordchk == strname)
             {
                 //cout<<wordchk;
-                if(wordchk == strname)
-                {
-
-                    chck=1;
-                    return 10;
-                }
+                chck=1;
+                priorityCount+=10;
             }
-    }
-    if(chck!=1)
+        }
+
+        ifstream dataFile9("9.txt",ios::in | ios::binary);
+        while(dataFile9 >> wordchk)
+        {
+            if(wordchk == strname)
+            {
+                //cout<<wordchk;
+                chck=1;
+                priorityCount+=9;
+            }
+        }
+        ifstream dataFile8("8.txt",ios::in | ios::binary);
+        while(dataFile8 >> wordchk)
+        {
+            if(wordchk == strname)
+            {
+                //cout<<wordchk;
+                chck=1;
+                priorityCount+=8;
+            }
+        }
+        ifstream dataFile7("7.txt",ios::in | ios::binary);
+        while(dataFile7 >> wordchk)
+        {
+            if(wordchk == strname)
+            {
+                //cout<<wordchk;
+                chck=1;
+                priorityCount+=7;
+            }
+        }
+        ifstream dataFile6("6.txt",ios::in | ios::binary);
+        while(dataFile6 >> wordchk)
+        {
+            if(wordchk == strname)
+            {
+                //cout<<wordchk;
+                chck=1;
+                priorityCount+=6;
+            }
+        }
+        ifstream dataFile5("5.txt",ios::in | ios::binary);
+        while(dataFile5 >> wordchk)
+        {
+            if(wordchk == strname)
+            {
+                //cout<<wordchk;
+                chck=1;
+                priorityCount+=5;
+            }
+        }
+        ifstream dataFile4("4.txt",ios::in | ios::binary);
+        while(dataFile4 >> wordchk)
+        {
+            if(wordchk == strname)
+            {
+                //cout<<wordchk;
+                chck=1;
+                priorityCount+=4;
+            }
+        }
+        ifstream dataFile3("3.txt",ios::in | ios::binary);
+        while(dataFile3 >> wordchk)
+        {
+            if(wordchk == strname)
+            {
+                //cout<<wordchk;
+                chck=1;
+                priorityCount+=3;
+            }
+        }
+    if(strname == ".")
     {
-        return -1;
+    storePriority[x]=priorityCount;
+      priorityCount=0;
+      x++;
     }
+
+    }
+
 
 }
 
 int main()
 {
-    int x = CountWord();
-    cout<<endl<<x<<endl;
+    CountWord();
+    //cout<<endl<<x<<endl;
+    cout<<endl<<storePriority[0]<<endl;
+    cout<<endl<<storePriority[1]<<endl;
 }
